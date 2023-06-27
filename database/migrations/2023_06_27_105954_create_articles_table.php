084d42ed664e5f320c26d9820d1f1ea5cad31d4d
Caen De Silva <caen@desilva.se>
2023-06-27 13:08:01 +0200

    Define article migration

--- database/migrations/2023_06_27_105954_create_articles_table.php
+++ database/migrations/2023_06_27_105954_create_articles_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('slug')->unique()->index();
+            $table->string('title');
+            $table->text('content');
+            $table->json('history')->nullable();
+            $table->json('meta')->nullable();
         });
     }
 
