4286785b0819440bba2d3808cf221fffdf6f8b7d
Caen De Silva <caen@desilva.se>
2023-06-27 13:09:13 +0200

    Define article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,46 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'slug',
+        'title',
+        'content',
+        'history',
+        'meta',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     */
+    protected $casts = [
+        'history' => 'array',
+        'meta' => 'array',
+    ];
+
+    /**
+     * Get the article route key name.
+     */
+    public function getRouteKeyName(): string {
+        return 'slug';
+    }
+
+    /**
+     * Get the article's history.
+     */
+    public function getHistory(): array
+    {
+        return $this->history ?? [];
+    }
+    
+    /**
+     * Get the article's metadata.
+     */
+    public function getMeta(): array
+    {
+        return $this->meta ?? [];
+    }
 }
