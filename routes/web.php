ef9ffbf4fd46d89a2f2bd8cd888fe9da4920bd17
Caen De Silva <caen@desilva.se>
2023-06-28 23:26:09 +0200

    Move up required routes

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+require __DIR__.'/auth.php';
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
 
 Route::resource('', \App\Http\Controllers\ArticleController::class);
 Route::get('/', function (\Illuminate\Foundation\Application $app) {
