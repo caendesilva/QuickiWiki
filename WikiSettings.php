188c3ed343a1b768c3f0c8a3aa422a499cef6bc5
Caen De Silva <caen@desilva.se>
2023-06-28 22:41:12 +0200

    Create base WikiSettings class

--- WikiSettings.php
+++ WikiSettings.php
@@ -1,4 +1,8 @@
 <?php
 
-// This file contains the settings for your Wiki.
+// This file contains the settings for your Wiki. Please edit as you see fit!
 
+final class WikiSettings {
+    const WIKI_NAME = 'QuickiWiki';
+    const WIKI_DESCRIPTION = 'A quick and easy wiki for your needs!';
+}
