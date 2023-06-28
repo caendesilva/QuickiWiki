9d27fb66b91069b44f79787c1553d52314ea1aa0
Caen De Silva <caen@desilva.se>
2023-06-28 23:05:10 +0200

    Revert "Use configured name in the seeder"
    This reverts commit 465dffa73cae1ae88efe1887f70e0413e1463400.

--- database/seeders/SiteSeeder.php
+++ database/seeders/SiteSeeder.php
@@ -17,7 +17,7 @@ class SiteSeeder extends Seeder
     {
         $index = Article::create([
             'slug' => 'index',
-            'title' => 'Welcome to the '.\WikiSettings::$WikiName.' Wiki!',
+            'title' => 'Welcome to the [[ SiteName ]] Wiki!',
             'content' => 'Welcome to QuickiWiki! This is the default homepage. You can edit it by clicking the "Edit" button.',
         ]);
 
