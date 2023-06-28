ae466bbef9173aa90273725e924ecf0c40b3f118
Caen De Silva <caen@desilva.se>
2023-06-28 22:50:10 +0200

    Display article header

--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -3,6 +3,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <article class="p-6 text-gray-900">
+                    <header>
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                            {{ $title }}
+                        </h2>
+                    </header>
                     {{ $content }}
                 </article>
             </div>
