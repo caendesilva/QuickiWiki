e75dd97de56909f5854739a21e94b43c39dde32b
Caen De Silva <caen@desilva.se>
2023-06-28 10:44:51 +0200

    Add contribution helpers

--- app/Models/Contribution.php
+++ app/Models/Contribution.php
@@ -13,6 +13,8 @@ class Contribution extends Model
      * The attributes that are mass assignable.
      */
     protected $fillable = [
+        'article_id',
+        'user_id',
         'message',
         'diff',
     ];
@@ -31,5 +33,37 @@ class Contribution extends Model
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Create a new contribution.
+     */
+    public static function log(Article $article, User $user, string $message, string $diff): static
+    {
+        return static::create([
+            'article_id' => $article->id,
+            'user_id' => $user->id,
+            'message' => $message,
+            'diff' => $diff,
+        ]);
+    }
+
+    /**
+     * Calculate the diff between two strings.
+     */
+    public static function diff(string $old, string $new): string
+    {
+        $old = explode("\n", $old);
+        $new = explode("\n", $new);
+
+        $diff = array_map(function ($old, $new) {
+            if ($old === $new) {
+                return $old;
+            }
+
+            return sprintf('<del>%s</del><ins>%s</ins>', $old, $new);
+        }, $old, $new);
+
+        return implode("\n", $diff);
     }
 }
