12b11fdb5aebb146fb249dfa7073db351fce4f54
Caen De Silva <caen@desilva.se>
2023-07-03 14:38:58 +0200

    Add dropdown menu to article toolbar

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -8,10 +8,32 @@
                             {{ $title }}
                         </h2>
                         
-                        <div class="text-sm text-gray-500">
+                        <div class="text-sm text-gray-500 flex items-start">
                             <a href="{{ route('articles.edit', $article) }}">
                                 {{ __('Edit') }}
                             </a>
+
+                            <x-dropdown>
+                                <x-slot:trigger>
+                                    <button class="ml-2 text-sm text-gray-500">
+                                        <span class="sr-only">{{ __('More actions') }}</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5" role="presentation" title="{{ __('More actions') }}">
+                                            <path d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z" />
+                                        </svg>
+                                    </button>
+                                </x-slot:trigger>
+
+                               <x-slot:content>
+                                   <x-dropdown-link href="{{ route('articles.destroy', $article) }}" onclick="event.preventDefault(); document.getElementById('delete-article-form').submit();">
+                                       {{ __('Delete') }}
+                                   </x-dropdown-link>
+
+                                   <form id="delete-article-form" action="{{ route('articles.destroy', $article) }}" method="POST" class="hidden">
+                                       @method('DELETE')
+                                       @csrf
+                                   </form>
+                               </x-slot:content>
+                            </x-dropdown>
                         </div>
                     </header>
                     <hr class="my-3">
