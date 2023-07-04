959bec79e2549a7217d462f5980f1077bf0b55d7
Caen De Silva <caen@desilva.se>
2023-07-04 15:30:46 +0200

    Link to the contributor's profile page

--- resources/views/article/contributions.blade.php
+++ resources/views/article/contributions.blade.php
@@ -27,7 +27,7 @@
                             <tbody>
                                 @foreach ($article->contributions as $contribution)
                                     <tr>
-                                        <td class="border px-4 py-2">{{ $contribution->user->name }}</td>
+                                        <td class="border px-4 py-2"><x-link href="{{ route('users.show', $contribution->user) }}">{{ $contribution->user->name }}</x-link></td>
                                         <td class="border px-4 py-2"><time datetime="{{ $contribution->created_at }}">{{ $contribution->created_at->format('H:i, d F Y') }}</time></td>
                                         <td class="border px-4 py-2">{{ $contribution->message }} <small>({{ $contribution->diff() }})</small></td>
                                     </tr>
