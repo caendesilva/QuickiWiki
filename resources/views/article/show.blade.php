590577bbb88c22e150697828a469380248728406
Caen De Silva <caen@desilva.se>
2023-07-08 10:41:45 +0200

    Remove overflow-hidden hiding dropdown

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="py-12 sm:py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white shadow-sm sm:rounded-lg">
                 <article class="p-6 text-gray-900">
                     <header class="flex justify-between">
                         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
