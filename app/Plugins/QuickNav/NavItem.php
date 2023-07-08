2e5f868222af4ccfe1185d5bd1d59c61690c35e9
Caen De Silva <caen@desilva.se>
2023-07-08 13:33:50 +0200

    Create NavItem.php

---
+++ app/Plugins/QuickNav/NavItem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Plugins\QuickNav;
+
+use Illuminate\Support\Facades\Route;
+
+class NavItem
+{
+    public readonly string $label;
+    public readonly string $destination;
+    public int $priority = 0;
+
+    public static function make(string $label, string $destination, int $priority = 0): static
+    {
+        return new static($label, $destination, $priority);
+    }
+
+    public function __construct(string $label, string $destination, int $priority = 0)
+    {
+        $this->label = $label;
+        $this->destination = $destination;
+        $this->priority = $priority;
+    }
+
+    public function resolve(): string
+    {
+        // If route exists, return route
+        if (Route::has($this->destination)) {
+            return route($this->destination);
+        }
+
+        // If route does not exist, return destination, assuming it is already a URL
+        return $this->destination;
+    }
+
+    public function isActive(): bool
+    {
+        return request()->is($this->destination);
+    }
+}
