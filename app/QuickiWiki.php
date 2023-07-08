e341e478041cac7e57db7bc1066cddf05dcc191d
Caen De Silva <caen@desilva.se>
2023-07-08 13:55:37 +0200

    Version v0.3.0-dev

--- app/QuickiWiki.php
+++ app/QuickiWiki.php
@@ -10,7 +10,7 @@ use WikiSettings;
  */
 class QuickiWiki
 {
-    public const VERSION = '0.2.0';
+    public const VERSION = '0.3.0-dev';
 
     /** @return array<\App\Plugins\QuickNav\NavItem> */
     public static function sidebarMenu(): array
