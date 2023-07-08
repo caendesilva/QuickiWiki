fed9d5d97b8206c023c3ed7a1a014d15178ceed9
Caen De Silva <caen@desilva.se>
2023-07-08 13:46:36 +0200

    Add new helper to generate the sidebar menu

--- app/QuickiWiki.php
+++ app/QuickiWiki.php
@@ -8,4 +8,12 @@ namespace App;
 class QuickiWiki
 {
     public const VERSION = '0.2.0';
+
+    /** @return array<\App\Plugins\QuickNav\NavItem> */
+    public static function sidebarMenu(): array
+    {
+        return collect(\WikiSettings::navigationItems())
+            ->filter(fn ($item) => $item->isVisible())
+            ->sortBy('priority')->toArray();
+    }
 }
